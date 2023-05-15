e3e8d8b72e3c134312823737f5f6e123161babb2
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-05-15 08:31:16 +0530

    react-native: hello-world

---
+++ tutorial01/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, SafeAreaView, Button} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World!</Text>
+        <Button title="Press me" />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
